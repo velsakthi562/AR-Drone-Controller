c5bf3b58361b51d68beaea67ce141792002f6911
vyvoxel <62131636+velsakthi562@users.noreply.github.com>
2021-06-14 14:29:58 +0530

    Commit

---
+++ Assets/Scripts/DroneController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneController : MonoBehaviour
+{
+    private Animator anim;
+
+    Vector3 speed = new Vector3(0.0f, 0.0f, 0.0f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.SetBool("TakeOff", true);
+    }
+
+    public void Move(float speedX, float speedZ)
+    {
+        speed.x = speedX;
+        speed.y = speedZ;
+
+        DroneMovement();
+    }
+
+
+
+    // Update is called once per frame
+    void DroneMovement()
+    {
+        transform.localPosition += speed * Time.deltaTime; 
+    }
+}
